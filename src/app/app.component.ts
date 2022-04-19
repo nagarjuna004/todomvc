37582cd60c925f77553e8ae1a4497427a0c650ff
Nagarjuna K <nagarjuna.arjun004@gmail.com>
2022-04-19 18:44:30 -0400

    todomvc in angular 12

---
+++ src/app/app.component.ts
@@ -0,0 +1,74 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ITodo } from "./todos/interfaces";
+import { Store } from "@ngrx/store";
+import {
+  addTodo,
+  changeFilterMode,
+  clearCompleted,
+} from "./todos/state/todo.actions";
+import { FILTER_MODES } from "./todos/constants/filter-modes";
+import { allTodos, filterMode } from "./todos/state/todo.selectors";
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+})
+export class AppComponent {
+  newTodo: string;
+  filter: FILTER_MODES;
+  todos: ITodo[];
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.select(filterMode).subscribe((state) => {
+      this.filter = state;
+      switch (state) {
+        case "Active":
+          this.store
+            .select(allTodos)
+            .subscribe(
+              (state) => (this.todos = state.filter((i) => !i.completed))
+            );
+          break;
+        case "Completed":
+          this.store
+            .select(allTodos)
+            .subscribe(
+              (state) => (this.todos = state.filter((i) => i.completed))
+            );
+          break;
+        default:
+          this.store
+            .select(allTodos)
+            .subscribe((state) => (this.todos = state));
+          break;
+      }
+    });
+  }
+
+  addTodo(event) {
+    event.preventDefault();
+    this.newTodo = event.target.value;
+    if (this.newTodo.length === 0) {
+      return;
+    }
+    this.store.dispatch(
+      addTodo({
+        text: this.newTodo,
+      })
+    );
+    this.newTodo = "";
+  }
+
+  setFilterMode(filterMode: FILTER_MODES) {
+    this.store.dispatch(
+      changeFilterMode({
+        mode: filterMode,
+      })
+    );
+  }
+
+  clearCompleted() {
+    this.store.dispatch(clearCompleted());
+  }
+}
